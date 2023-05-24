bd32b9e77bf8a32dc896ae50e4c96cf4ed8dce7f
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-05-25 02:33:33 +0500

    Improve styling

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
 import { getDetails } from '../redux/detailSlice';
 import '../styles/Details.css';
 
